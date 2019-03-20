fd072bda28715d5fc069bb200c7c6c1a9e291372
stefaniahernandez <47236121+stefaniahernandez@users.noreply.github.com>
2019-03-19 21:50:59 -0700

    Add files via upload

---
+++ Assignment 5/js/dom-practice.js
@@ -0,0 +1,35 @@
+/*eslint-env browser*/
+
+//STEP 1
+/*
+function firstButton() {
+    "use strict";
+    window.alert("I have been clicked.");
+}
+firstButton();
+*/
+//STEP 2
+/*
+var secondButton = document.getElementById("button2");
+secondButton.onclick = function () {
+    "use strict";
+    window.alert("I have been clicked.");
+};
+*/
+//STEP 3
+
+
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
